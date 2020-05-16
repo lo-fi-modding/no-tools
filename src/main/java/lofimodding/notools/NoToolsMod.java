c861ebd27994a72489415050e7ebb161a3a83ed2
Corey Frenette <corey@narwhunderful.com>
2020-05-16 16:18:33 -0300

    Add tooltip and global disable config

--- src/main/java/lofimodding/notools/NoToolsMod.java
+++ src/main/java/lofimodding/notools/NoToolsMod.java
@@ -7,8 +7,11 @@ import net.minecraft.item.Items;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.Tag;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.data.LanguageProvider;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.UseHoeEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -37,6 +40,7 @@ public class NoToolsMod {
     forgeBus.addListener(this::onBreakSpeed);
     forgeBus.addListener(this::onUseHoe);
     forgeBus.addListener(this::onAttackEntity);
+    forgeBus.addListener(this::onTooltip);
   }
 
   public static ResourceLocation loc(final String path) {
@@ -50,7 +54,7 @@ public class NoToolsMod {
   private void onBreakSpeed(final PlayerEvent.BreakSpeed event) {
     final Item held = event.getPlayer().getHeldItemMainhand().getItem();
 
-    if(Config.DISABLE_USE.get() && DISABLED_TOOLS.contains(held)) {
+    if(Config.ENABLED.get() && Config.DISABLE_USE.get() && DISABLED_TOOLS.contains(held)) {
       event.setCanceled(true);
     }
   }
@@ -58,7 +62,7 @@ public class NoToolsMod {
   private void onUseHoe(final UseHoeEvent event) {
     final Item held = event.getContext().getItem().getItem();
 
-    if(Config.DISABLE_USE.get() && DISABLED_TOOLS.contains(held)) {
+    if(Config.ENABLED.get() && Config.DISABLE_USE.get() && DISABLED_TOOLS.contains(held)) {
       event.setCanceled(true);
     }
   }
@@ -66,16 +70,39 @@ public class NoToolsMod {
   private void onAttackEntity(final AttackEntityEvent event) {
     final Item held = event.getPlayer().getHeldItemMainhand().getItem();
 
-    if(Config.DISABLE_USE.get() && DISABLED_TOOLS.contains(held)) {
+    if(Config.ENABLED.get() && Config.DISABLE_USE.get() && DISABLED_TOOLS.contains(held)) {
       event.setCanceled(true);
+    }
+  }
+
+  private void onTooltip(final ItemTooltipEvent event) {
+    final Item held = event.getItemStack().getItem();
+
+    if(Config.ENABLED.get() && Config.DISABLE_USE.get() && DISABLED_TOOLS.contains(held)) {
+      event.getToolTip().add(new TranslationTextComponent("no-tools.disable_use"));
     }
   }
 
   private void gatherData(final GatherDataEvent event) {
     final DataGenerator gen = event.getGenerator();
 
+    if(event.includeClient()) {
+      gen.addProvider(new EnglishLang(gen));
+    }
+
     if(event.includeServer()) {
       gen.addProvider(new ItemTagProvider(gen));
+    }
+  }
+
+  public static class EnglishLang extends LanguageProvider {
+    public EnglishLang(final DataGenerator gen) {
+      super(gen, MOD_ID, "en_us");
+    }
+
+    @Override
+    protected void addTranslations() {
+      this.add("no-tools.disable_use", "This tool has been disabled by No Tools!");
     }
   }
 
